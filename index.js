d49d331334ea853b3f9cae73f4058d89ca994a18
Arshad Kazmi <arshadkazmi42@gmail.com>
2018-12-30 23:02:40 +0530

    Code formatting - Test script added in package.json - Output json added in readme

--- index.js
+++ index.js
@@ -8,27 +8,23 @@ function flatten (target,  children, opts) {
     var delimiter = opts.delimiter || '.'
     var maxDepth = opts.maxDepth 
     var output = {}
-
-  
+ 
     function step (object, prev) {
  
       var type = Object.prototype.toString.call(object)
-
       var isArray = Array.isArray(object)
 
       var isObject = (
         type === '[object Object]' ||
         type === '[object Array]'
-        )
+      )
 
-      if( isObject && !isArray ){
+      if (isObject && !isArray) {
 
         let newKey = prev ? prev: delimiter;
-
         output[newKey] = { ...object };
 
         step(object[children], newKey+ delimiter+children );
-
         delete output[newKey][[children]];
       }
 
@@ -36,14 +32,12 @@ function flatten (target,  children, opts) {
 
         Object.keys(object).forEach(function (key) {
 
-            let newKey = prev ? prev+ delimiter+ key : key
-            step(object[key], newKey )
-
-            })
-        }
-
+          let newKey = prev ? `${prev}${delimiter}${key}` : key
+          step(object[key], newKey )
+        });
+      }
     }
+
     step(target)
     return output
   }
-
